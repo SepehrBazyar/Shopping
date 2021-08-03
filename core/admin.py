db54556f99dcf6b33c9e5618e9cd38b9a1c97272
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-08-03 23:03:26 +0430

    change user admin default for show phone number field

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import *
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    """
+    Customization Admin Pane of User Model for Show Phone Number & Sync with User Name
+    """
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
 
 class BasicAdmin(admin.ModelAdmin):
     """
