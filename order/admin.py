0c76e6d7724cd92cf26c25bc1e55fbf03b17f129
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-08-04 22:24:35 +0430

    fix bugs and remove modify in validator file because create bug

--- order/admin.py
+++ order/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from core.admin import *
 from product.admin import DiscountAdmin
 from .models import *
 
 # Register your models here.
+class OrderItemInlineAdmin(admin.TabularInline):
+    """
+    Create New Order Item Instance in Order Admin Page with Tabular Inline Model
+    """
+
+    model = OrderItem
+    fields = [('product', 'count')]
+    exclude = ['deleted', 'delete_timestamp']
+    verbose_name_plural = _("Order Items")
+    extra = 1
+
+
 @admin.register(DiscountCode)
 class DiscountCodeAdmin(DiscountAdmin):
     """
@@ -15,3 +28,38 @@ class DiscountCodeAdmin(DiscountAdmin):
     fieldsets = [(None, {'fields': ('code',)}),] + DiscountAdmin.fieldsets
     list_display = DiscountAdmin.list_display + ['code']
     list_filter  = DiscountAdmin.list_filter  + ['code']
+
+
+@admin.register(Order)
+class OrderAdmin(BasicAdmin):
+    """
+    Manage Order Class Model and Show Fields in Panel Admin
+    """
+
+    exclude = BasicAdmin.exclude + ['discount']
+    fieldsets = (
+        (None, {
+            'fields': (('customer', 'status'), ('total_price', 'final_price'), ('code',)),
+        }),
+    )
+    list_display = ('__str__', 'customer', 'total_price', 'final_price', 'status', 'discount')
+    list_filter = ('status',)
+    search_fields = ('customer',)
+    ordering = ('-id',)
+    inlines = [OrderItemInlineAdmin]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(BasicAdmin):
+    """
+    Manage Order Items Class Model and Show Fields in Panel Admin
+    """
+
+    fieldsets = (
+        (None, {
+            'fields': (('order'), ('product', 'count'))
+        }),
+    )
+    list_display = ('__str__', 'order', 'product', 'count')
+    search_fields = ('order', 'product')
+    ordering = ('-id',)
--- order/validators.py
+++ order/validators.py
@@ -12,8 +12,6 @@ class CountValidator:
     def __call__(self, count: int):
         if count > self.product_item.inventory:
             raise ValidationError(_("Inventory of Product Item isn't Enough!"))
-        self.product_item.inventory -= count
-        self.product_item.save()
 
 
 class DiscountCodeValidator:
@@ -28,7 +26,5 @@ class DiscountCodeValidator:
         if self.discount_code.count() > 0:
             if customer in self.discount_code[0].users.all():
                 raise ValidationError(_("This Discount Code has Expired for You!"))
-            else:
-                self.discount_code[0].users.add(customer)
         else:
             raise ValidationError(_("Not Found Matching Any Discount Code!"))
