1bec431e08c37e5b983f0477c1858d0a1c712e0b
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-08-08 14:37:43 +0430

    check authenticated user for login and register pages

--- customer/views.py
+++ customer/views.py
@@ -18,6 +18,11 @@ class CustomerLoginView(LoginView):
     authentication_form = CustomerLoginForm
     template_name = "customer/login.html"
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse("product:categories"))
+        return super().get(request, *args, **kwargs)
+
 
 class CustomerLogoutView(LogoutView):
     """
@@ -36,7 +41,13 @@ class CustomerRegisterView(generic.FormView):
     template_name = "customer/register.html"
     success_url = reverse_lazy("product:categories")
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse("product:categories"))
+        return super().get(request, *args, **kwargs)
+
     def form_valid(self, form):
-        Customer.objects.create_user(username=form.data["username"],
-                                    password=form.data["password1"])
+        user = Customer.objects.create_user(username=form.data["username"],
+                                            password=form.data["password1"])
+        login(self.request, user)
         return redirect(reverse("product:categories"))
