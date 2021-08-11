5c99d5ac056b4c59b6f9f6a2d83f9d00247c5677
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-08-11 23:57:26 +0430

    create new permission for check user is owner or is staff

--- core/permissions.py
+++ core/permissions.py
@@ -14,3 +14,12 @@ class IsStaffUser(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user.is_staff
+
+
+class IsOwnerUser(permissions.BasePermission):
+    """
+    Permission for Check User Send Request is Owner of Object or No
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.customer or request.user.is_staff
